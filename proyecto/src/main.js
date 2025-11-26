67610a410322d134b7c7e871f7d82a6ec69afe3e
Cristiancgt-byte <cristian.gambat@uniagustiniana.edu.co>
2025-11-26 06:17:14 +0000

    segundo commit

--- proyecto/src/main.js
+++ proyecto/src/main.js
@@ -1,8 +1,12 @@
 // src/main.js
 import { mostrarRegistro } from './register.js';
 import { mostrarLogin } from './login.js';
+import { mostrarMVP } from './mvp.js';
+import { mostrarUser } from './user.js';
+import { mostrarAdmin } from './admin.js';
+
+
+mostrarAdmin();
 
 
 
-// ✅ Mostrar la página de registro al iniciar la app
-mostrarRegistro();
